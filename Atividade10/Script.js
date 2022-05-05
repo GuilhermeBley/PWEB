21141f5bd66a279861bbc3d1ed639a6f6dc1a252
Guilherme Bley <69880922+GuilhermeBley@users.noreply.github.com>
2022-05-04 22:10:24 -0300

    Atividade 10
    2022-05-04

---
+++ Atividade10/Script.js
@@ -0,0 +1,19 @@
+
+// Calculo média
+function GenerateAlunos(){
+
+    let obj1 = function(ra, nome){
+        this.ra = ra,
+        this.nome = nome
+    };
+
+    let obj2 = {
+        ra : 1234,
+        nome : "Guiherme"
+    };
+
+    let obj3 = new Object();
+    obj3.ra = 1234;
+    obj3.nome ="Guilherme";
+}
+
